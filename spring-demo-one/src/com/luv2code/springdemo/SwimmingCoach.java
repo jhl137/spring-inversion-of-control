45b56a8b2056d076731818cd382e0b6450a10e5c
jhl137 <jonathanhongli@gmail.com>
2021-05-08 22:31:01 -0400

    spring demo

---
+++ spring-demo-one/src/com/luv2code/springdemo/SwimmingCoach.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo;
+
+public class SwimmingCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "swim 2hrs freestyle";
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
